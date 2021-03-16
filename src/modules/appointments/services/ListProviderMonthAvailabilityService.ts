5af0293c123e9ad3da7910b58e64da37be399e7e
Danylo Oliveira <danylo.oliveira@codecs.com.br>
2021-03-16 17:49:19 -0300

    Atualizando

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -31,7 +31,7 @@ class ListProviderMonthAvailabilityService {
       },
     );
     
-    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month -1));
    
     const eachDayArray = Array.from(
       {
@@ -41,7 +41,7 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
-      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+      const compareDate = new Date(year, month -1, day, 23, 59, 59);
 
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
@@ -50,7 +50,7 @@ class ListProviderMonthAvailabilityService {
       return {
         day,
         available:
-          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+          isAfter(compareDate, new Date()) && appointmentsInDay.length <10,
       };
     });
 
